7be86bd201083a2cbc82ae86acab1205cc21d197
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2015-12-30 17:48:42 -0600

    repaired dependencies

--- src/server.js
+++ src/server.js
@@ -6,14 +6,16 @@ import uriUtil from 'mongodb-uri'
 const app = express()
 const port = process.env.PORT || 8000
 
-var mongodbUri = process.env.MONGOLAB_URL || 'mongodb://localhost/robot-db'
-var mongooseUri = uriUtil.formatMongoose(mongodbUri)
+let mongodbUri = process.env.MONGOLAB_URL || 'mongodb://localhost/robot-db'
+let mongooseUri = uriUtil.formatMongoose(mongodbUri)
 
 mongoose.connect(mongooseUri)
 
-var conn = mongoose.connection
+let conn = mongoose.connection
 
 conn.on('error', console.error.bind(console, 'connection error:'))
 
-app.use('/movement', api)
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+conn.once('open', function() {
+  app.use('/movement', api)
+  app.listen(port, () => console.log(`Server listening on port ${port}`))
+});
